2ec46f5ba8e9dfd9f10e4a6028ccd16322c4afd8
Feliverse <114632980+Feliverse@users.noreply.github.com>
2025-11-23 19:12:15 -0400

    update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "navy.onrender.com" }
+  config.action_mailer.default_url_options = { host: "https://navy-trtg.onrender.com" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
