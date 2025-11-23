6b6675b66d423b04fa72429d40f3b27c3db470b9
Feliverse <114632980+Feliverse@users.noreply.github.com>
2025-11-23 19:58:01 -0400

    debuging to deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,4 +7,9 @@ bundle install
 yarn install --frozen-lockfile
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+# Do NOT run database migrations during the build step on Render.
+# Running migrations in the build phase often fails because the build
+# environment doesn't have access to the production database.
+# Use Render's "Release Command" (or a manual/one-off job) to run
+# `bundle exec rake db:migrate` after the deploy succeeds.
+# bundle exec rake db:migrate
